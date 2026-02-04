f8578002974305a68cf572b2c37385993340387f
ankitvchauhan <ankit.ss5072.xib@gmail.com>
2026-02-04 20:54:40 +0530

    exp3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/home';
 import Projects from './pages/projects';
 import Contact from './pages/contact';
 import AppNavbar from './components/Navbar';
